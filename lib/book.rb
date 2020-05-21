c5936aa1387e054a6235beb5f58be0fbd4ae36a8
harmonious-cable-1188 <rtyenriques@gmail.com>
2020-05-21 20:29:14 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -24,4 +24,16 @@ def page_count
   @page_count
 end
 
+def genre=(genre)
+  @genre = genre
+end
+
+def genre
+  @genre
+end
+
+def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+
 end
